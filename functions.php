46275cd75b4213bf0cec8593012fa85ca76c2fd8
Michal Madera <michalmadera@Mac-mini-Michal.local>
2021-07-14 13:05:30 +0200

    Task 1

--- functions.php
+++ functions.php
@@ -1,5 +1,17 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-wp_enqueue_style( 'parent-style',get_template_directory_uri().'/style.css' );
+/**
+ * 
+ *  Functions.php - to child themes Twenty Twenty
+ * 
+ */
+
+// Add custom style css to theme parent and custom style
+
+function add_twentytwenty_child_style() {
+    // Include parent style
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    // Include custom style
+    wp_enqueue_style( 'child-custom-style', get_stylesheet_directory_uri().'/assets/css/custom.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'add_twentytwenty_child_style' );
